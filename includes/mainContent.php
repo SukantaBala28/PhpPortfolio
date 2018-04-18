c3d65e1f92c542e275f4a7bf814b066fdc83a0b1
Sukanta Bala <sukantabalacste2016@gmail.com>
2018-04-18 19:44:23 +0600

    Index file added

---
+++ includes/mainContent.php
@@ -0,0 +1,35 @@
+
+<div class="mainContent">
+	<div class="row m-0">
+		<div class="sidenav mynav">
+		  <a href="#about">About</a>
+		  <a href="#services">Services</a>
+		  <a href="#clients">Clients</a>
+		  <a href="#contact">Contact</a>
+		</div>
+		<div class="col-md-9 offset-md-3 col-sm-12">
+			<div class="d-block d-md-none mobileTopbar fixed-top">
+				<div class="row">
+					<div class="col">
+						<span>Sukanta Bala</span>
+					</div>
+					<div class="col text-right">
+						<span>Top bar</span>
+					</div>
+				</div>
+			</div>
+			<div class="body-content">
+				<h2>Sidebar</h2>
+			  <p>This sidebar is of full height (100%) and always shown.</p>
+			  <p>Scroll down the page to see the result.</p>
+			  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+			  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+			  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+			  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+     
